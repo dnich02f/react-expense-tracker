84a5184ececc54edf9f18243e65188b9ecf15bfb
Darryl Nichols <darryl.nichols@simfoods.com>
2023-08-16 13:19:23 -0600

    Added comments for Context API

--- react-expense-tracker/src/context/GlobalState.js
+++ react-expense-tracker/src/context/GlobalState.js
@@ -1,38 +1,42 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
-// Initial state
+// Initial state for the global context
 const initialState = {
   transactions: []
 }
 
-// Create context
+// Create a context with the initial state
 export const GlobalContext = createContext(initialState);
 
-// Provider component
+// Provider component that wraps the app and provides global state and actions
 export const GlobalProvider = ({ children }) => {
+  // Use the useReducer hook to manage state and actions
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  // Actions
+  // Define actions that modify the state
   function deleteTransaction(id) {
     dispatch({
-      type: 'DELETE_TRANSACTION',
-      payload: id
+      type: 'DELETE_TRANSACTION', // Action type for deleting a transaction
+      payload: id // Data to be sent with the action (transaction ID to be deleted)
     });
   }
 
   function addTransaction(transaction) {
     dispatch({
-      type: 'ADD_TRANSACTION',
-      payload: transaction
+      type: 'ADD_TRANSACTION', // Action type for adding a transaction
+      payload: transaction // Data to be sent with the action (new transaction data)
     });
   }
 
-  return (<GlobalContext.Provider value={{
-    transactions: state.transactions,
-    deleteTransaction,
-    addTransaction
-  }}>
-    {children}
-  </GlobalContext.Provider>);
+  // Provide the state and actions to the components through the context
+  return (
+    <GlobalContext.Provider value={{
+      transactions: state.transactions, // State data: list of transactions
+      deleteTransaction, // Action function to delete a transaction
+      addTransaction // Action function to add a transaction
+    }}>
+      {children} {/* Render the child components */}
+    </GlobalContext.Provider>
+  );
 }
